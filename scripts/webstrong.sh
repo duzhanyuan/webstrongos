42a37345d86a4e2c5d8d74f409cbdf43b70433bf
mateuszpijanowski <mateuszpijanowski0@gmail.com>
2019-08-25 12:57:44 +0200

    Add scripts

---
+++ scripts/webstrong.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Created by Mateusz Pijanowski (WebStrong)
+
+clear
+echo -e ' \e[1m__          __  _     _____ _                          ____   _____ '
+echo ' \ \        / / | |   / ____| |                        / __ \ / ____|'
+echo '  \ \  /\  / /__| |__| (___ | |_ _ __ ___  _ __   __ _| |  | | (___  '
+echo '   \ \/  \/ / _ \ `_ \\___ \| __| `__/ _ \| `_ \ / _` | |  | |\___ \ '
+echo '    \  /\  /  __/ |_) |___) | |_| | | (_) | | | | (_| | |__| |____) |'
+echo '     \/  \/ \___|_.__/_____/ \__|_|  \___/|_| |_|\__, |\____/|_____/ '
+echo '                                                  __/ |              '
+echo '                                                 |___/               ' 
+
+echo
+echo -e "                         Name: \e[32mWebStrongOS\e[39m"
+echo -e "                         Version: \e[32m2.0 AMD64\e[39m"
+echo -e "                         Basic: \e[32mDebian 9\e[39m"
+echo -e "                         Date core: \e[32m25082019\e[39m"
+echo
+echo -e " GitHub:\e[21m https://github.com/mateuszpijanowski/webstrongos"
+echo -e " \e[1mPowered by:\e[21m https://WebStrong.pl"
+echo " 2019 © by Mateusz Pijanowski"
+echo
